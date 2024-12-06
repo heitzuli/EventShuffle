7da45612b6fcdbc4b2784998f12f900f8362ab4d
heitzuli <heidi.poels@gmail.com>
2024-12-06 11:43:55 +0200

    feat: initial commit, Spring initializer

---
+++ src/main/java/com/heitzuli/eventshuffle/EventShuffleApplication.java
@@ -0,0 +1,13 @@
+package com.heitzuli.eventshuffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventShuffleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventShuffleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heitzuli/eventshuffle/EventShuffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heitzuli.eventshuffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventShuffleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
