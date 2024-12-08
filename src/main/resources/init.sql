e322d4bdc2051c668cbb7e31d0f5a913134acfd3
heitzuli <heidi.poels@gmail.com>
2024-12-08 11:43:48 +0200

    fix: server crashes with foreign keys

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -9,13 +9,11 @@ CREATE TABLE IF NOT EXISTS events (
 CREATE TABLE IF NOT EXISTS dates (
     date_id INT AUTO_INCREMENT PRIMARY KEY,
     event_id INT,
-    date DATE,
-    FOREIGN KEY (event_id) REFERENCES events(id)
+    date DATE
 );
 
 CREATE TABLE IF NOT EXISTS votes (
     vote_id INT AUTO_INCREMENT PRIMARY KEY,
     date_id INT,
-    person VARCHAR(250),
-    FOREIGN KEY (date_id) REFERENCES dates(id)
+    person VARCHAR(250)
 );
